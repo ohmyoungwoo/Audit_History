1915ced7be0dda1cf720a34760214f6a921b1149
ohmyoungwoo <myoungou.oh@lge.com>
2024-08-23 10:45:11 +0900

    get_query_list 함수 이동

--- domain/question/question_crud.py
+++ domain/question/question_crud.py
@@ -5,49 +5,13 @@ from domain.question.question_schema import QuestionCreate, QuestionUpdate
 from model.models import Question, User, Answer
 from sqlalchemy.orm import Session
 from sqlalchemy import and_
-from domain.question.query_list import get_query_list
-
+#from domain.question.query_list import get_query_list
 
 def get_question_list(db: Session, skip: int = 0, limit: int = 10, keyword: str = ''):
     question_list = db.query(Question)
     if keyword:
         question_list = get_query_list(db, keyword)
-        #search = keyword.split()
-        #search = '%%{}%%'.format(keyword_list)  # keyword는 화면에서 전달 받은 값
-        ##search = '%%{}%%'.format(keyword)  # keyword는 화면에서 전달 받은 값
-        #search =[]
-        #search = keyword.split() 
-        #for key in keyword_list:
-        #    search.append('%%{}%%'.format(key))
         
-        #print(search)
-        #sub_query = db.query(Answer.question_id, Answer.content, User.username)\
-        #    .outerjoin(User, and_(Answer.user_id == User.id)).subquery()
-        """
-        question_list = question_list \
-            .outerjoin(User) \
-            .filter(Question.subject.ilike(search)|   # 진단 제목
-                    Question.content.ilike(search)|   # 진단 상세 내용
-                    Question.auditor1.ilike(search)|    # 진단자 이름1
-                    Question.auditor2.ilike(search)|    # 진단자 이름2
-                    Question.auditor3.ilike(search)|    # 진단자 이름3
-                    Question.company.ilike(search)|     # 회사 (ex) LGE, 신성델타 ...
-                    Question.region.ilike(search)|      # 사업장 
-                    Question.production.ilike(search)|  # 제품군
-                    Question.audit_type.ilike(search)|   # 진단유형
-                    Question.audit_year_start.ilike(search)|   # Year_start
-                    Question.audit_year_end.ilike(search)   # Year_end
-                    #sub_query.c.content.ilike(search) | # 답변내용, 
-                    #sub_query.c.username.ilike(search) # 답변 작성자
-            #.outerjoin(sub_query, and_ (sub_query.c.question_id == Question.id)) \
-            )
-            # sub_query.c.question_id 에서 c 는 서브쿼리의 조회 항목이며, 
-            # sub_query.c.question_id 는 서브쿼리의 조회 항목 중 question_id를 의미함
-            # and_ 는 sqlalchemy 의 특이한 함수???
-        """
-    
-    #question_list = get_query_list(db, keyword)    
-    
     total = question_list.distinct().count()
     question_list = question_list.order_by(Question.audit_date.desc()) \
         .offset(skip).limit(limit).distinct().all() # 리스트 정렬 (진단일자 기준)
@@ -155,5 +119,48 @@ def delete_question(db: Session, db_question: Question):
     
     db.delete(db_question)
     db.commit()
+    
+
+def get_query_list(db: Session, keyword: str = ''):
+   
+    question_list = db.query(Question)
+    
+    if keyword:
+        keyword_list = keyword.split()
+        search_list =[]
+        for key in keyword_list:
+            search_list.append('%%{}%%'.format(key))
+        
+        for search in search_list:
+            if search == search_list[0]:
+                query = question_list.filter(Question.subject.ilike(search)           # 진단 제목
+                                                    |Question.content.ilike(search)           # 진단 상세 내용
+                                                    |Question.auditor1.ilike(search)          # 진단자 이름1
+                                                    |Question.auditor2.ilike(search)          # 진단자 이름2
+                                                    |Question.auditor3.ilike(search)          # 진단자 이름3
+                                                    |Question.company.ilike(search)           # 회사 (ex) LGE, 신성델타 ...
+                                                    |Question.region.ilike(search)            # 사업장 
+                                                    |Question.production.ilike(search)        # 제품군
+                                                    |Question.audit_type.ilike(search)        # 진단유형
+                                                    |Question.audit_year_start.ilike(search)  # Year_start
+                                                    |Question.audit_year_end.ilike(search)    # Year_end
+                )
+            
+            else:
+                query = query.union(question_list.filter(Question.subject.ilike(search)           # 진단 제목
+                                                                        |Question.content.ilike(search)           # 진단 상세 내용
+                                                                        |Question.auditor1.ilike(search)          # 진단자 이름1
+                                                                        |Question.auditor2.ilike(search)          # 진단자 이름2
+                                                                        |Question.auditor3.ilike(search)          # 진단자 이름3
+                                                                        |Question.company.ilike(search)           # 회사 (ex) LGE, 신성델타 ...
+                                                                        |Question.region.ilike(search)            # 사업장 
+                                                                        |Question.production.ilike(search)        # 제품군
+                                                                        |Question.audit_type.ilike(search)        # 진단유형
+                                                                        |Question.audit_year_start.ilike(search)  # Year_start
+                                                                        |Question.audit_year_end.ilike(search)    # Year_end
+                ))
+            
+
+    return query
     
     
